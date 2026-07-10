219acbf1455d1deef8d35fec44122d8cfd36c604
nikeshjaintms <nikeshjaintms@gmail.com>
2026-07-10 11:45:11 +0530

    BASE URL ADDED

---
+++ src/BaseUrl.js
@@ -0,0 +1,24 @@
+export const V_URL = "http://localhost:5000/api";
+export const PAY = "Payroll System";
+export const M_STORE = "Main Store";
+export const P_STORE = "Project Store";
+
+// export const ERP = "Project Fabrication";
+export const ERP = "Structural Fabrication";
+export const PIPING = "Piping Fabrication";
+export const PLAN = 'Planning Engineer';
+export const QC = 'QC Engineer';
+export const M_CON = 'Material Controller';
+export const PRODUCTION = 'Production Engineer';
+export const WELD = 'Welding Engineer';
+export const PAINT = 'Painting Engineer';
+export const QA = 'QA Engineer';
+// export const PIPING_PROCUREMENT = "Procurement_PIPING"
+export const CENTRAL_PLAN = "Central Planning";
+export const Procurement = "Procurement";
+
+
+// export const PMS = 'Project Fabrication'; techomaxcontactus@123
+export const PMS = 'Structural Fabrication';
+export const PO_PLAN = "Structural Procurement";
+export const PIPING_PLAN = "Piping Procurement";    
